a2ded6c3a952445c910429000d0f979bbb8800f3
Nicolo' Ferri <irr3f92@gmail.com>
2023-06-07 14:59:44 +0200

    Terzo esercizio

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction._3;
+
+public class PrintYourNameAndSurname {
+    public static void main (String[] args) {
+        System.out.print("Nicolo' ");
+        System.out.println("Ferri");
+    }
+}
